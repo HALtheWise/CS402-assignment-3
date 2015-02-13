e48cc3bd1813a264866e3b6af7893b360ebf44bd
Eric Miller <eric@legoaces.org>
2015-02-13 13:06:37 -0500

    Added music and stuff

--- Assets/Scripts/GameState.js
+++ Assets/Scripts/GameState.js
@@ -23,14 +23,19 @@ function OnGUI(){
 	if(gameOver){
 		if (surviveTime == 0){
 			surviveTime = Time.time - gameObject.GetComponent(DifficultyController).startTime;
+			audio.Play();
 		}
 		GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height/2), gameOverScreen);
-		if(GUI.Button(Rect(Screen.width/2-50, Screen.height/2, 100, 35), "Play Again?")){
+		if(GUI.Button(Rect(Screen.width/2-120, Screen.height/2, 100, 35), "Play Again?")){
 			Debug.Log("Playing again");
 			Start();
 			sub.GetComponent(FishScript).Start();
 			gameObject.GetComponent(DifficultyController).Start();
 			gameObject.GetComponent(WallGenerator).Start();
+		}
+		if(GUI.Button(Rect(Screen.width/2+20, Screen.height/2, 100, 35), "Main Title")){
+			Debug.Log("Main title");
+			Application.LoadLevel("titleScene");
 		}
 		
 		GUI.Label(Rect(Screen.width/2-100, Screen.height/2 + 50, 200, 50), "You lasted " + surviveTime + " seconds!");
---
+++ Assets/Scripts/MenuScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var logo:Texture;
+function OnGUI(){
+	GUILayout.BeginVertical();
+		GUILayout.Box(logo);
+		if (GUILayout.Button("Play Game")){
+			Application.LoadLevel("gameScene");
+		}
+		if (GUILayout.Button("Story")){
+			Application.LoadLevel("storyScene");
+		}
+	GUILayout.EndVertical();
+}
