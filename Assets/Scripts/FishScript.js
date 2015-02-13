d6c0de1655e4c9d9b09ce609e3e0e66eb97ef0d7
Eric Miller <eric@legoaces.org>
2015-02-12 21:53:28 -0500

    Made movements more aggressive

--- Assets/Scripts/FishScript.js
+++ Assets/Scripts/FishScript.js
@@ -9,10 +9,8 @@ var downSpeed:float = 0.02;
 
 function Update () {
 	if (Input.GetKey("space")){
-		//transform.position.y += .03;
 		rigidbody.velocity.y += upSpeed;
 	} else{
-		//transform.position.y -= 0.02;
 		rigidbody.velocity.y -= downSpeed;
 	}
 }
