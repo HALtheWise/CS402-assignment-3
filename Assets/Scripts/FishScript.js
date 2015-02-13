219b0fe60538f3b6b539ad9ee87a9a9c2a5276bb
Eric Miller <eric@legoaces.org>
2015-02-12 21:52:10 -0500

    Switched to acceleration control

--- Assets/Scripts/FishScript.js
+++ Assets/Scripts/FishScript.js
@@ -4,10 +4,15 @@ function Start () {
 
 }
 
+var upSpeed:float = 0.03;
+var downSpeed:float = 0.02;
+
 function Update () {
 	if (Input.GetKey("space")){
-		transform.position.y += .03;
+		//transform.position.y += .03;
+		rigidbody.velocity.y += upSpeed;
 	} else{
-		transform.position.y -= 0.02;
+		//transform.position.y -= 0.02;
+		rigidbody.velocity.y -= downSpeed;
 	}
 }
