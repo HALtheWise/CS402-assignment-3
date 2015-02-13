044f249b8be241ee8795e15e64203cc8f5e1043d
Eric Miller <eric@legoaces.org>
2015-02-12 23:23:34 -0500

    Bug fix

--- Assets/Scripts/Autodelete.js
+++ Assets/Scripts/Autodelete.js
@@ -6,6 +6,6 @@ function Start () {
 
 function Update () {
 	if (transform.position.x < -10 || transform.position.x > 10){
-		GameObject.Destroy(GameObject);
+		GameObject.Destroy(gameObject);
 	}
 }
