8cabf586f8bf77fb3cb134c28f6b26059cdd093f
Eric Miller <eric@legoaces.org>
2015-02-12 22:34:29 -0500

    Created Wall Generator

---
+++ Assets/Scripts/Wall Generator.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var topWall:GameObject;
+var bottomWall:GameObject;
+
+var lastTop:float;
+var lastBottom:float;
+var lastOpening:float;
+var lastTrend:float;
+var openingSize:float;
+
+function Update () {
+	if(Input.GetKeyDown("s")){
+		spawnWalls(3, -3);
+	}
+}
+
+function spawnWalls(top:float, bottom:float) {
+	GameObject.Instantiate(topWall, new Vector3(0,top,0), Quaternion.identity);
+	GameObject.Instantiate(bottomWall, new Vector3(0,bottom,0), Quaternion.identity);
+}
